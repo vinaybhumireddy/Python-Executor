1e648a5e345f7cbee8dae65c09ce6f26a1c83818
Vinay <53284302+vinaybhumireddy@users.noreply.github.com>
2020-02-08 20:53:08 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+
+namespace PythonExecutor
+{
+    class Program
+    {
+         static void Main(string[] args)
+        {
+            string res = ScriptRunner.RunFromCmd(@"Python scripts path", "args");
+            Console.WriteLine(res);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ ScriptRunner.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Net;
+using System.Security;
+using System.Text;
+
+namespace PythonExecutor
+{
+    public class ScriptRunner
+    {
+        //args separated by spaces
+        public static string RunFromCmd(string rCodeFilePath, string args)
+        {
+            string file = rCodeFilePath;
+            string result = string.Empty;
+
+            try
+            {
+                SecureString securePwd = new NetworkCredential("", "password").SecurePassword;
+                var info = new ProcessStartInfo(@"Python program Path")
+                {
+                    Arguments = rCodeFilePath,
+                    RedirectStandardInput = false,
+                    RedirectStandardOutput = true,
+                    UseShellExecute = false,
+                    RedirectStandardError=true,
+                    CreateNoWindow = true,
+                };
+
+                using (var proc = new Process())
+                {
+                   
+                    proc.StartInfo = info;
+                    proc.Start();
+                    proc.WaitForExit();
+                    Console.WriteLine(proc.ExitCode);
+                    if (proc.ExitCode == 0)
+                    {
+                        result = proc.StandardOutput.ReadToEnd();
+                    }
+                    else
+                    {
+                        result = proc.StandardError.ReadToEnd();
+                    }
+                }
+                return result;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("R Script failed: " + result, ex);
+            }
+        }
+       
+    }
+
+}
